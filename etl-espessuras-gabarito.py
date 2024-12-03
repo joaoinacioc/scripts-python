8757ead4d5e0c50356af3a67d08ad9efe170d37b
João Vitor Inácio Claro <joaovitor.inacioc@gmail.com>
2024-12-03 13:17:52 -0300

    Create etl-espessuras-gabarito.py

---
+++ etl-espessuras-gabarito.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import os
+
+diretorio_novos_dados = r'H:/Business Intelligence/Clientes/AMP/Redução/Dados Não Extraídos/'
+arquivos = os.listdir(diretorio_novos_dados)
+
+cabeçalhos = ['Canal', 'DI', 'Data', 'Prod Acumulada', 'Prod DI']
+df_dados_espessuras = pd.DataFrame()
+
+def extrair_dados(df_skip_rows, info_skip_rows,arquivos):   # Extraindo informações do diretório
+    df_combinado = pd.DataFrame()
+    
+    while df_skip_rows <= 63: 
+        df = pd.read_excel(diretorio_novos_dados+arquivos, sheet_name='Medições', usecols='A,C:M', skiprows=df_skip_rows, nrows=17)
+        
+        df_info = pd.read_excel(diretorio_novos_dados+arquivos, sheet_name='Medições', usecols='D:F,H,L', skiprows=info_skip_rows, nrows=2)
+        df_info.columns = cabeçalhos
+        
+        df['Canal'] = int(df_info['Canal'][0])
+        di = str(df_info['DI'][1])
+        data = df_info['Data'].dt.date[0]
+        prod_acumulada = float(df_info['Prod Acumulada'][0])
+        prod_DI = float(df_info['Prod DI'][0])
+    
+        df['DI'] = di
+        df['Data'] = data
+        df['Prod Acumulada'] = prod_acumulada
+        df['Prod DI'] = prod_DI
+        
+        df_skip_rows += 20
+        info_skip_rows += 20
+        
+        df_combinado = pd.concat([df_combinado, df], ignore_index=True)
+    
+    return df_combinado
+
+
+diretorio_dados_coletados = r'H:/Business Intelligence/Clientes/AMP/Redução/Dados Extraídos/'
+diretorio_arquivo_final = r'H:/Business Intelligence/Clientes/AMP/Redução/Base_Dados_Espessuras.xlsx'
+
+for arquivo in range(len(arquivos)):
+    df_temporario = extrair_dados(3,0,arquivos[arquivo])
+    df_dados_espessuras = pd.concat([df_dados_espessuras, df_temporario], ignore_index=True)
+
+df_dados_espessuras.to_excel(diretorio_arquivo_final, sheet_name='Dados Espessuras')
